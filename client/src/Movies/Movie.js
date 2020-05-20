5b3c6f8943f55149282ff1c728d04557af32103a
Angelg10 <angelguzman97@gmail.com>
2020-05-20 16:19:47 -0400

    update movie button

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Route, useHistory, useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import MovieCard from "./MovieCard";
 
 function Movie({ addToSavedList }) {
